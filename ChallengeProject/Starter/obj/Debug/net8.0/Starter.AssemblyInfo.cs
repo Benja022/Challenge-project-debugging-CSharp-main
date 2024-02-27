1c1d86573045cfb72f85a5cfd71d77048bcfa622
Benja022 <113633123+Benja022@users.noreply.github.com>
2024-02-27 17:49:31 +0100

    Actualización de versiones de ensamblado en ChallengeProject y Starter.

--- ChallengeProject/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ ChallengeProject/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e93c517eea67c6a2a87e627eae5830ff28391e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cfbe8357588f6c7de738d47aa1fee0694b63e1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
