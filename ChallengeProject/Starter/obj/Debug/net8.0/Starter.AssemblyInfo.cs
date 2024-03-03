f91d32801788e9aa5c2955bc8caa06f107a91087
Benja022 <113633123+Benja022@users.noreply.github.com>
2024-03-03 11:26:45 +0100

    Updating

--- ChallengeProject/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ ChallengeProject/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6c0f7b07d7782cf4b6644260a539f860f9ced3d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32136233b4b505e7ba0918984498814652d2fc26")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
