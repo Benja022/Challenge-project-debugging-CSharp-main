aa2a5980ad8deccda592e8578a13af50d172517c
Benja022 <113633123+Benja022@users.noreply.github.com>
2024-03-06 09:26:24 +0100

    Actualizadas las versiones de ensamblado en AssemblyInfo.cs y AssemblyInfoInputs.cache en ChallengeProject/Final y ChallengeProject/Starter.

--- ChallengeProject/Final/obj/Debug/net8.0/Final.AssemblyInfo.cs
+++ ChallengeProject/Final/obj/Debug/net8.0/Final.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Final")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32136233b4b505e7ba0918984498814652d2fc26")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f91d32801788e9aa5c2955bc8caa06f107a91087")]
 [assembly: System.Reflection.AssemblyProductAttribute("Final")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Final")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ChallengeProject/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ ChallengeProject/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32136233b4b505e7ba0918984498814652d2fc26")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f91d32801788e9aa5c2955bc8caa06f107a91087")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
