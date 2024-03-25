964e81acedf6cf8815ed71ff4e229c3caadcf4de
Benja022 <113633123+Benja022@users.noreply.github.com>
2024-03-25 22:20:16 +0100

    Actualización de versiones de ensamblado en el proyecto Final y Starter

--- ChallengeProject/Final/obj/Debug/net8.0/Final.AssemblyInfo.cs
+++ ChallengeProject/Final/obj/Debug/net8.0/Final.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Final")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+397c47d23b9d0e01773a051f3af573f46de6f70e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+827fb551ff38a94ca7fc028fb619653543a34a44")]
 [assembly: System.Reflection.AssemblyProductAttribute("Final")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Final")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
